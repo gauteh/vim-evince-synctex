d9f6029368305d92c9831055da81495b97010608
Gaute Hope <eg@gaute.vetsj.com>
2018-02-21 12:03:56 +0100

    Quote file name before opening in evince

--- bin/evince_backward.sh
+++ bin/evince_backward.sh
@@ -2,10 +2,10 @@
 
 d=$(dirname $(readlink -f $0))
 
-if ! ps aux | grep evince_backward_search | grep $2 ; then 
-  $d/evince_backward_search $1 "gvim --servername $2 --remote-silent '+%l<Enter>' %f" &
+if ! ps aux | grep evince_backward_search | grep $2 ; then
+  $d/evince_backward_search "$1" "gvim --servername $2 --remote-silent '+%l<Enter>' %f" &
 
-  evince $1 
+  evince "$1"
 
   kill %1
 fi
